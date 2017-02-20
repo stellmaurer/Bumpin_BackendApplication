d4dff8fbb5e65678783a3473f597d15df97417de
Stephen Ellmaurer <stellmaurer@gmail.com>
2017-02-19 23:52:35 -0800

    Queries working

--- main.go
+++ main.go
@@ -2,30 +2,30 @@ package main
 
 import (
 	"encoding/json"
-	//"fmt"
+	"fmt"
 	"net/http"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
-	//"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
 func main() {
 	http.HandleFunc("/hello", hello)
-
-	http.HandleFunc("/dynamoDB", queryDynamo)
-
+	http.HandleFunc("/tables", tables)
+	http.HandleFunc("/person", person)
+	http.HandleFunc("/party", party)
 	http.ListenAndServe(":8080", nil)
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello!"))
+	w.Write([]byte("hello!\n"))
 }
 
-func queryDynamo(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Querying DynamoDB now."))
-	data, err := queryPersonInDynamoDB()
+func tables(w http.ResponseWriter, r *http.Request) {
+	data, err := getTables()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -33,10 +33,44 @@ func queryDynamo(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	json.NewEncoder(w).Encode(data)
+	//w.Write([]byte(data))
 }
 
+func person(w http.ResponseWriter, r *http.Request) {
+	data, err := getPerson()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	var person PersonData
+	jsonErr := dynamodbattribute.UnmarshalMap(data, &person)
+	if jsonErr != nil {
+		http.Error(w, jsonErr.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(person)
+	//w.Write([]byte(person.Name))
+}
 
-func queryPersonInDynamoDB() (string, error) {
+func party(w http.ResponseWriter, r *http.Request) {
+	data, err := getParty()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	var party PartyData
+	jsonErr := dynamodbattribute.UnmarshalMap(data, &party)
+	if jsonErr != nil {
+		http.Error(w, jsonErr.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(party)
+	//w.Write([]byte(person.Name))
+}
+
+func getTables() (string, error) {
 	svc := dynamodb.New(session.New(&aws.Config{Region: aws.String("us-west-2")}))
 	result, err := svc.ListTables(&dynamodb.ListTablesInput{})
 	if err != nil {
@@ -48,7 +82,9 @@ func queryPersonInDynamoDB() (string, error) {
 	    tables = tables + *table
 	}
 	return tables, nil
-	/*
+}
+
+func getPerson() (map[string]*dynamodb.AttributeValue, error) {
 	type ItemGetter struct {
 		DynamoDB dynamodbiface.DynamoDBAPI
 	}
@@ -68,67 +104,73 @@ func queryPersonInDynamoDB() (string, error) {
 	attributeValue.SetS("12345699033")
 	getItemInput.SetKey(map[string]*dynamodb.AttributeValue{"facebookID": &attributeValue})
 	getItemOutput, err2 := getter.DynamoDB.GetItem(&getItemInput)
-	return getItemOutput.GoString(), err2
-	*/
+	return getItemOutput.Item, err2
 }
 
-type personData struct {
-	BlackoutList struct {
-		NS []string `json:"NS"`
-	} `json:"blackoutList"`
-	FacebookID struct {
-		S string `json:"S"`
-	} `json:"facebookID"`
-	InvitedTo struct {
-		NS []int64 `json:"NS"`
-	} `json:"invitedTo"`
-	IsABarOwner struct {
-		BOOL bool `json:"BOOL"`
-	} `json:"isABarOwner"`
-	IsMale struct {
-		BOOL bool `json:"BOOL"`
-	} `json:"isMale"`
-	Name struct {
-		S string `json:"S"`
-	} `json:"name"`
-	PartyHostFor struct {
-		NS []int64 `json:"NS"`
-	} `json:"partyHostFor"`
-}
-
-/*
-func query(city string) (weatherData, error) {
-	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=YOUR_API_KEY&q=" + city)
+func getParty() (map[string]*dynamodb.AttributeValue, error) {
+	type ItemGetter struct {
+		DynamoDB dynamodbiface.DynamoDBAPI
+	}
+	// Setup
+	var getter = new(ItemGetter)
+	var config *aws.Config = &aws.Config{Region: aws.String("us-west-2")}
+	sess, err := session.NewSession(config)
 	if err != nil {
-		return weatherData{}, err
+		fmt.Println("err")
 	}
-
-	defer resp.Body.Close()
-
-	var d weatherData
-
-	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
-		return weatherData{}, err
-	}
-
-	return d, nil
+	var svc *dynamodb.DynamoDB = dynamodb.New(sess)
+	getter.DynamoDB = dynamodbiface.DynamoDBAPI(svc)
+	// Finally
+	var getItemInput = dynamodb.GetItemInput{}
+	getItemInput.SetTableName("Party")
+	var attributeValue = dynamodb.AttributeValue{}
+	attributeValue.SetN("1")
+	getItemInput.SetKey(map[string]*dynamodb.AttributeValue{"partyID": &attributeValue})
+	getItemOutput, err2 := getter.DynamoDB.GetItem(&getItemInput)
+	return getItemOutput.Item, err2
 }
-*/
 
-type partyData struct {
-	AddressLine1        string  `json:"addressLine1"`
-	AddressLine2        string  `json:"addressLine2"`
-	City                string  `json:"city"`
-	Country             string  `json:"country"`
-	Description         string  `json:"description"`
-	DrinksProvided      bool    `json:"drinksProvided"`
-	EndTime             string  `json:"endTime"`
-	FeeForDrinks        bool    `json:"feeForDrinks"`
-	Latitude            float32 `json:"latitude"`
-	Longitude           float32 `json:"longitude"`
-	PartyID             int64   `json:"partyID"`
-	PartyName           string  `json:"partyName"`
-	StartTime           string  `json:"startTime"`
-	StateProvinceRegion string  `json:"stateProvinceRegion"`
-	ZipCode             int16   `json:"zipCode"`
+type PersonData struct {
+		BlackoutList []string `json:"blackoutList"`
+		FacebookID string 		`json:"facebookID"`
+		InvitedTo []uint64 		`json:"invitedTo"`
+		IsABarOwner bool 			`json:"isABarOwner"`
+		IsMale bool 					`json:"isMale"`
+		Name string 					`json:"name"`
+		PartyHostFor []uint64 `json:"partyHostFor"`
+}
+
+type PartyData struct {
+	AddressLine1        string  	`json:"addressLine1"`
+	AddressLine2        string  	`json:"addressLine2"`
+	City                string  	`json:"city"`
+	Country             string  	`json:"country"`
+	Details	         		string  	`json:"details"`
+	DrinksProvided      bool    	`json:"drinksProvided"`
+	EndTime             string  	`json:"endTime"`
+	FeeForDrinks        bool    	`json:"feeForDrinks"`
+	Hosts								[]Host 		`json:"hosts"`
+	Invitees						[]Invitee `json:"invitees"`
+	Latitude            float64 	`json:"latitude"`
+	Longitude           float64 	`json:"longitude"`
+	PartyID             uint64   	`json:"partyID"`
+	StartTime           string  	`json:"startTime"`
+	StateProvinceRegion string  	`json:"stateProvinceRegion"`
+	Title           		string  	`json:"title"`
+	ZipCode             uint32  	`json:"zipCode"`
+}
+
+type Host struct {
+	FacebookID	string 	`json:"facebookID"`
+	IsMainHost 	bool 		`json:"isMainHost"`
+	Name				string 	`json:"name"`
+}
+
+type Invitee struct {
+	AtParty 		bool		`json:"atParty"`
+	FacebookID	string	`json:"facebookID"`
+	IsMale			bool		`json:"isMale"`
+	Name				string	`json:"name"`
+	Rating			string	`json:"rating"`
+	Status			string 	`json:"status"`
 }
